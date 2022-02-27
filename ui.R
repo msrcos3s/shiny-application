b435c19637b175b52f9270be726705a0d65dcba5
Marcos Medeiros <93498592+msrcos3s@users.noreply.github.com>
2022-02-27 15:26:46 -0300

    Update ui.R
    Updated

--- ui.R
+++ ui.R
@@ -4,18 +4,29 @@ library(plotly)
 
 shinyUI(fluidPage(
         
-        titlePanel("ICU Occupancy Rate"),
-        
+        titlePanel("Taxas de Ocupacao Leitos UTI-Covid no Estado de SP"),
+        h5 ("Criado em R"),
+        h6 ("por Marcos S. Medeiros"),
+        theme = shinythemes::shinytheme('superhero'),
         sidebarLayout(
                 sidebarPanel(
-                        uiOutput("Region")
+                        helpText("Qual regiao deseja visualizar?"),
+                        uiOutput("Region"),
+                        hr(),
+                        helpText("O grafico mostra a taxa de ocupacao de leitos.",
+                                 hr(),
+                                 img(src = "taxa.jpg", height = 30, width = 240),
+                                 hr(),
+                                 "Selecione uma area do grafico para ampliar em detalhes.",
+                                 hr(),
+                                 img(src = "msrcos3s.png", height = 120, width = 240))
                 ),
                 
                 
                 mainPanel(
-                        h1("State of Sao Paulo by Region"),
-                        h2("Real Time Interactive Plot"),
-                        h6 ("linked to https://github.com/seade-R/dados-covid-sp/raw/master/data/plano_sp_leitos_internacoes.csv"),
+                        h3("Estado de Sao Paulo por Regiao"),
+                        h4("Grafico interativo em tempo real"),
+                        h5 ("dados brutos disponiveis em https://github.com/seade-R/dados-covid-sp/raw/master/data/plano_sp_leitos_internacoes.csv"),
                         h3(textOutput("text1")),
                         plotlyOutput("distPlot")
                 )
