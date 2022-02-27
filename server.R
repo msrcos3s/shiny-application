e64a83b1306f6d222867cf6493536b153e2458f2
Marcos Medeiros <93498592+msrcos3s@users.noreply.github.com>
2022-02-27 15:25:55 -0300

    Update server.R
    Updated

--- server.R
+++ server.R
@@ -11,19 +11,20 @@ shinyServer(
                 data$datahora <- as.Date(data$datahora, "%Y-%m-%d")
                 
                 output$Region <- renderUI({
-                        selectInput("Region", "Choose a Region:", as.list(unique(data$nome_drs)), 
+                        selectInput("Region", "Escolha uma regiao:", as.list(unique(data$nome_drs)), 
                                     selected = levels(data$nome_drs)[23] ) 
                 })
                 
                 output$text1 <- renderText({
-                        paste("You have selected: ", input$Region)
+                        paste("Voce selecionou: ", input$Region)
                 })
                 
                 
                 output$distPlot <- renderPlotly({
                         dt <- data[data$nome_drs == input$Region, ]
                         
-                        p <- ggplot(data = dt, aes(x = datahora, y = ocupacao_leitos_ultimo_dia)) + geom_line()
+                        p <- ggplot(data = dt, aes(x = datahora, y = ocupacao_leitos_ultimo_dia)) + theme_bw() + geom_line() + geom_hline(yintercept = 75, linetype="dashed", color="red")+
+                                geom_hline(yintercept = 50, linetype="dashed", color="yellow") + geom_area(fill = "lightblue", alpha=0.2)
                         
                         p <- ggplotly(p)
                 })
